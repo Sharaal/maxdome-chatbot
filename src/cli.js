006c6f39f0609f2a07c9ecf9c96ae8e28e4dadf1
Christoph Herrmann <developer@dragonprojects.de>
2016-10-08 09:54:06 +0200

    Rename adapter to platform

--- src/cli.js
+++ src/cli.js
@@ -1,22 +1,22 @@
 import dotenv from 'dotenv';
 import program from 'commander';
 
-import adapters from './adapters';
 import generalCommands from './commands';
 import generalModules from './modules';
+import platforms from './platforms';
 
 dotenv.config({ silent: true });
 
 program
-  .option('-a, --adapter [name]', 'Use the specified adapter [botbuilder]', 'botbuilder')
+  .option('-p, --platform [name]', 'Use the specified platform [botbuilder]', 'botbuilder')
   .option('-b, --botbuilderConnector [name]', 'Use the specified connector of botbuilder [console]', 'console')
   .parse(process.argv);
 
-const adapter = program.adapter;
+const platform = program.platform;
 const botbuilderConnector = program.botbuilderConnector;
 
-if (adapters[adapter]) {
-  adapters[adapter]({ botbuilderConnector, generalCommands, generalModules });
+if (platforms[platform]) {
+  platforms[platform]({ botbuilderConnector, generalCommands, generalModules });
 } else {
-  throw new Error(`Unknown adapter '${adapter}', available adapters: ${Object.keys(adapters).join(', ')}`);
+  throw new Error(`Unknown platform '${platform}', available platforms: ${Object.keys(platforms).join(', ')}`);
 }
